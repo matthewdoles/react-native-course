e096b627ca35b16824baff8717d609a98379870f
Matthew Doles <matteddied@gmail.com>
2020-04-17 11:02:09 -0400

    2.4 - State, Events, & Outputting Lists

--- rn-course-goals/App.js
+++ rn-course-goals/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import { StyleSheet, TextInput, View, Button } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, TextInput, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState();
+  const [courseGoals, setCourseGoals] = useState([]);
+
+  const goalInputHandler = (eneteredText) => {
+    setEnteredGoal(eneteredText);
+  };
+
+  const addGoalHandler = () => {
+    setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
+  };
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput placeholder='Course Goal' style={styles.input} />
-        <Button title='ADD' />
+        <TextInput
+          placeholder='Course Goal'
+          style={styles.input}
+          onChangeText={goalInputHandler}
+          value={enteredGoal}
+        />
+        <Button title='ADD' onPress={addGoalHandler} />
       </View>
-      <View></View>
+      <View>
+        {courseGoals.map((goal) => <Text key={goal}>{goal}</Text>)}
+      </View>
     </View>
   );
 }
