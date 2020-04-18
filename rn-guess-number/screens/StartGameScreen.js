1be928f7d48d04b9124a7ff4ddfd77d113915cae
Matthew Doles <matteddied@gmail.com>
2020-04-18 12:00:18 -0400

    4.3 - StartGameScreen Layout & Styling

---
+++ rn-guess-number/screens/StartGameScreen.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native';
+
+const StartGameScreen = (props) => {
+  return (
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a New Game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title='Confirm' />
+          </View>
+          <View style={styles.button} >
+            <Button title='Reset' />
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  inputContainer: {
+    width: 300,
+    maxWidth: '80%',
+    alignItems: 'center',
+    shadowColor: 'black',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.26,
+    shadowRadius: 6,
+    elevation: 7,
+    padding: 20,
+    backgroundColor: 'white',
+    borderRadius: 10,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    width: '100%',
+    justifyContent: 'space-between',
+    paddingHorizontal: 15,
+  },
+  button: {
+    width: '40%',
+  },
+});
+
+export default StartGameScreen;
