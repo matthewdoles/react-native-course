ba83390e67700f1e2d3b0cfb777623374968deca
Matthew Doles <matteddied@gmail.com>
2020-04-21 20:13:17 -0400

    6.5 - Header Buttons, Passing Data to Meal Detail Screen

--- rn-meals-app/screens/CategoryMealScreen.js
+++ rn-meals-app/screens/CategoryMealScreen.js
@@ -19,7 +19,14 @@ const CategoryMealScreen = (props) => {
         duration={itemData.item.duration}
         complexity={itemData.item.complexity}
         affordability={itemData.item.affordability}
-        onProps={() => {}}
+        onSelectMeal={() => {
+          props.navigation.navigate({
+            routeName: 'MealDetail',
+            params: {
+              mealId: itemData.item.id,
+            },
+          });
+        }}
       />
     );
   };
@@ -49,7 +56,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'white'
+    backgroundColor: 'white',
   },
 });
 
--- rn-meals-app/screens/MealDetailsScreen.js
+++ rn-meals-app/screens/MealDetailsScreen.js
@@ -1,16 +1,37 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
-const MealDetailsScreen = (props) => {
+import HeaderButton from '../components/HeaderButton';
+import { MEALS } from '../data/dummy-data';
+
+const MealDetailScreen = (props) => {
+  const mealId = props.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
   return (
     <View style={styles.screen}>
-      <Text>The Meal Details Screen!</Text>
-      <Button 
+      <Text>{selectedMeal.title}</Text>
+      <Button
         title='Go Back to Categories!'
         onPress={() => props.navigation.popToTop()}
       />
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = (navigationData) => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal = MEALS.find((meal) => meal.id === mealId);
+
+  return {
+    headerTitle: selectedMeal.title,
+    headerRight: () => (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title='Favorite' iconName='ios-star' onPress={() => {}} />
+      </HeaderButtons>
+    ),
+  };
 };
 
 const styles = StyleSheet.create({
@@ -21,4 +42,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default MealDetailsScreen;
+export default MealDetailScreen;
