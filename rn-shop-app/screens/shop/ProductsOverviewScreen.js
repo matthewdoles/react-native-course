4f0652b0023b9ec7eb30261cab6447c815a462ab
Matthew Doles <matteddied@gmail.com>
2020-04-23 14:10:26 -0400

    8.2 - ProductsOverview content & ProductItem component

---
+++ rn-shop-app/screens/shop/ProductsOverviewScreen.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { FlatList, Text, StyleSheet } from 'react-native';
+import { useSelector } from 'react-redux';
+
+import ProductItem from '../../components/shop/ProductItem';
+
+const ProductsOverviewScreen = (props) => {
+  const products = useSelector((state) => state.products.availableProducts);
+
+  return (
+    <FlatList
+      data={products}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {}}
+          onAddToCart={() => {}}
+        />
+      )}
+    />
+  );
+};
+
+ProductsOverviewScreen.navigationOptions = {
+  headerTitle: 'All Products',
+};
+
+const styles = StyleSheet.create({});
+
+export default ProductsOverviewScreen;
