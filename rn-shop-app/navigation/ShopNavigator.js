80b4371dafb62f8642a45fffe65ed6b5f1d28647
Matthew Doles <matteddied@gmail.com>
2020-04-24 15:47:02 -0400

    8.6 - OrderScreen/OrderItems content & styling, Redux logic for Orders

--- rn-shop-app/navigation/ShopNavigator.js
+++ rn-shop-app/navigation/ShopNavigator.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import { Platform } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
+import { Ionicons } from '@expo/vector-icons';
 
 import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
+import OrdersScreen from '../screens/shop/OrdersScreen';
 import Colors from '../constants/Colors';
+
+const defaultNavOptions = {
+  headerStyle: {
+    backgroundColor: Platform.OS == 'android' ? Colors.primary : '',
+  },
+  headerTintColor: Platform.OS == 'android' ? 'white' : Colors.primary,
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold',
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans',
+  },
+};
 
 const ProductsNavigator = createStackNavigator(
   {
@@ -14,19 +31,47 @@ const ProductsNavigator = createStackNavigator(
     Cart: CartScreen,
   },
   {
-    defaultNavigationOptions: {
-      headerStyle: {
-        backgroundColor: Platform.OS == 'android' ? Colors.primary : '',
-      },
-      headerTintColor: Platform.OS == 'android' ? 'white' : Colors.primary,
-      headerTitleStyle: {
-        fontFamily: 'open-sans-bold',
-      },
-      headerBackTitleStyle: {
-        fontFamily: 'open-sans',
-      },
+    defaultNavigationOptions: defaultNavOptions,
+    navigationOptions: {
+      drawerIcon: (drawerConfig) => (
+        <Ionicons
+          name={Platform.OS === 'android' ? 'md-list' : 'ios-list'}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
     },
   }
 );
 
-export default createAppContainer(ProductsNavigator);
+const OrdersNavigator = createStackNavigator(
+  {
+    Orders: OrdersScreen,
+  },
+  {
+    defaultNavigationOptions: defaultNavOptions,
+    navigationOptions: {
+      drawerIcon: (drawerConfig) => (
+        <Ionicons
+          name={Platform.OS === 'android' ? 'md-cart' : 'ios-cart'}
+          size={23}
+          color={drawerConfig.tintColor}
+        />
+      ),
+    },
+  }
+);
+
+const ShopNavigator = createDrawerNavigator(
+  {
+    Products: ProductsNavigator,
+    Orders: OrdersNavigator,
+  },
+  {
+    contentOptions: {
+      activeTintColor: Colors.primary,
+    },
+  }
+);
+
+export default createAppContainer(ShopNavigator);
--- rn-shop-app/store/reducers/cart.js
+++ rn-shop-app/store/reducers/cart.js
@@ -1,4 +1,5 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from '../actions/cart';
+import { ADD_ORDER } from '../actions/order';
 import CartItem from '../../models/cart-item';
 
 const initialState = {
@@ -52,6 +53,8 @@ export default (state = initialState, action) => {
         items: updatedCartItems,
         totalAmount: state.totalAmount - cartItem.productPrice,
       };
+    case ADD_ORDER:
+      return initialState;
     default:
       return state;
   }
