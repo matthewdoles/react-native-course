cc7504606f74b526e90e4af0b1b720d7c76099cd
Matthew Doles <matteddied@gmail.com>
2020-05-01 15:09:29 -0400

    12.2 - Adding & Displaying Places

--- rn-great-places-app/screens/NewPlaceScreen.js
+++ rn-great-places-app/screens/NewPlaceScreen.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  ScrollView,
+  View,
+  Button,
+  Text,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
+import { useDispatch } from 'react-redux';
+
+import Colors from '../constants/Colors';
+import * as placeActions from '../store/places-actions';
 
 const NewPlaceScreen = (props) => {
+  const [title, setTitle] = useState('');
+  const dispatch = useDispatch();
+
+  const titleChangeHandler = (text) => {
+    setTitle(text);
+  };
+
+  const savePlaceHandler = () => {
+    dispatch(placeActions.addPlace(title));
+    props.navigation.goBack();
+  };
+
   return (
-    <View>
-      <Text>NewPlaceScreen</Text>
-    </View>
+    <ScrollView>
+      <View style={styles.form}>
+        <Text style={styles.label}>Title</Text>
+        <TextInput
+          style={styles.textInput}
+          onChangeText={titleChangeHandler}
+          value={title}
+        />
+        <Button
+          title='Save Place'
+          color={Colors.primary}
+          onPress={savePlaceHandler}
+        />
+      </View>
+    </ScrollView>
   );
 };
 
@@ -13,6 +48,21 @@ NewPlaceScreen.navigationOptions = {
   headerTitle: 'New Place',
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  form: {
+    margin: 30,
+  },
+  label: {
+    fontSize: 18,
+    marginBottom: 15,
+  },
+  textInput: {
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    marginBottom: 15,
+    paddingVertical: 4,
+    paddingHorizontal: 2,
+  },
+});
 
 export default NewPlaceScreen;
