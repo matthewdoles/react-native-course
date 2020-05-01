b96f7737f0b6ede897507f3d82460b400a576eac
Matthew Doles <matteddied@gmail.com>
2020-05-01 14:15:00 -0400

    12.1 - Great Places App init

---
+++ rn-great-places-app/screens/NewPlaceScreen.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const NewPlaceScreen = (props) => {
+  return (
+    <View>
+      <Text>NewPlaceScreen</Text>
+    </View>
+  );
+};
+
+NewPlaceScreen.navigationOptions = {
+  headerTitle: 'New Place',
+};
+
+const styles = StyleSheet.create({});
+
+export default NewPlaceScreen;
