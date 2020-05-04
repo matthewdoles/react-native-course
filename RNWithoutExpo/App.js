163f21fc9165d0a037b0f7c60f063ebbdb492bba
Matthew Doles <matteddied@gmail.com>
2020-05-04 13:35:08 -0400

    13.1 - React Native CLI App, Bringing in Native Modules

---
+++ RNWithoutExpo/App.js
@@ -0,0 +1,62 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  Button,
+} from 'react-native';
+import ImagePicker from 'react-native-image-picker';
+
+const App = () => {
+  const pickImage = () => {
+    const options = {
+      title: 'Select Avatar',
+      customButtons: [{ name: 'fb', title: 'Choose Photo from Facebook' }],
+      storageOptions: {
+        skipBackup: true,
+        path: 'images',
+      },
+    };
+    
+    /**
+     * The first arg is the options object for customization (it can also be null or omitted for default options),
+     * The second arg is the callback which sends object: response (more info in the API Reference)
+     */
+    ImagePicker.showImagePicker(options, (response) => {
+      console.log('Response = ', response);
+    
+      if (response.didCancel) {
+        console.log('User cancelled image picker');
+      } else if (response.error) {
+        console.log('ImagePicker Error: ', response.error);
+      } else if (response.customButton) {
+        console.log('User tapped custom button: ', response.customButton);
+      } else {
+        console.log(response.uri)
+      }
+    });
+  }
+
+  return (
+   <View style={styles.screen}>
+     <Button title='Take Image' onPress={pickImage} />
+   </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
+
+export default App;
